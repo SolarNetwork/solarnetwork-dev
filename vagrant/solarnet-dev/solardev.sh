9a1f8516851cd38e549ed55d5de8faa29f22cf0f
Matt Magoffin <git+matt@msqr.us>
2015-02-02 12:48:18 +1300

    Generate an Eclipse Team Project Set file out of the projects in git, so they can be imported by user after Eclipse starts.

--- vagrant/solarnet-dev/solardev.sh
+++ vagrant/solarnet-dev/solardev.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Setup .xinitrc to launch Fluxbox
 if [ ! -e ~/.xinitrc ]; then
@@ -18,41 +18,27 @@ fi
 EOF
 fi
 
+# Setup main SolarNet database
+psql -d solarnetwork -U solarnet -c 'select count(*) from solarnet.sn_node' >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+	echo 'Creating solarnet database tables...'
+	cd ~/git/solarnetwork-central/net.solarnetwork.central.datum/defs/sql/postgres
+	# for some reason, plv8 often chokes on the inline comments, so strip them out
+	sed -e '/^\/\*/d' -e '/^ \*/d' postgres-init-plv8.sql |psql -d solarnetwork -U solarnet
+	psql -d solarnetwork -U solarnet -f postgres-init.sql
+fi
+
+# Setup unit test database
+psql -d solarnet_unittest -U solarnet_test -c 'select count(*) from solarnet.sn_node' >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+	echo 'Creating solarnet_unittest database tables...'
+	cd ~/git/solarnetwork-central/net.solarnetwork.central.datum/defs/sql/postgres
+	# for some reason, plv8 often chokes on the inline comments, so strip them out
+	sed -e '/^\/\*/d' -e '/^ \*/d' postgres-init-plv8.sql |psql -d solarnet_unittest -U solarnet_test
+	psql -d solarnet_unittest -U solarnet_test -f postgres-init.sql
+fi
+
 # Setup Eclipse
-eclipseDownload=/var/tmp/eclipse.tgz
-eclipseDownloadMD5=7385aaff4872800153ebc90d8c92e707
-eclipseDownloadHash=
-if [ -e "$eclipseDownload" ]; then
-	eclipseDownloadHash=`md5sum $eclipseDownload |cut -d' ' -f1`
-fi
-if [ "$eclipseDownloadHash" != "$eclipseDownloadMD5" ]; then
-	echo 'Downloading Eclipse Luna...'
-	#wget -c -O $eclipseDownload -nv http://mirrors.ibiblio.org/eclipse/technology/epp/downloads/release/luna/SR1a/eclipse-jee-luna-SR1a-linux-gtk.tar.gz
-	curl -C - -s -S -o $eclipseDownload http://mirrors.ibiblio.org/eclipse/technology/epp/downloads/release/luna/SR1a/eclipse-jee-luna-SR1a-linux-gtk.tar.gz
-fi
-if [ -e "$eclipseDownload" ]; then
-	eclipseDownloadHash=`md5sum $eclipseDownload |cut -d' ' -f1`
-fi
-if [ ! -d ~/eclipse -a -e "$eclipseDownload" ]; then
-	if [ "$eclipseDownloadHash" = "$eclipseDownloadMD5" ]; then
-		echo "Installing Eclipse Luna..."
-		tar -C ~/ -xzf "$eclipseDownload"
-	else
-		echo 'Eclipse Luna not completely downloaded, cannot install.'
-	fi
-fi
-
-# Install EGit
-# ls -1d ~/eclipse/features/org.eclipse.egit* >/dev/null 2>&1
-# if [ $? -ne 0 -a -x ~/eclipse/eclipse ]; then
-# 	echo 'Installing EGit...'
-# 	~/eclipse/eclipse -application org.eclipse.equinox.p2.director \
-# 		-repository http://download.eclipse.org/releases/luna \
-# 		-installIU org.eclipse.egit.feature.group \
-# 		-tag AddEGit \
-# 		-destination ~/eclipse/ -profile SDKProfile
-# fi
-
 # Checkout SolarNetwork sources
 for proj in build external common central node; do
 	if [ ! -d ~/git/solarnetwork-$proj ]; then
@@ -132,20 +118,171 @@ if [ ! -e ~/git/solarnetwork-node/net.solarnetwork.node.test/environment/local/l
 		~/git/solarnetwork-node/net.solarnetwork.node.test/environment/local/
 fi
 
-psql -d solarnetwork -U solarnet -c 'select count(*) from solarnet.sn_node' >/dev/null 2>&1
-if [ $? -ne 0 ]; then
-	echo 'Creating solarnet database tables...'
-	cd ~/git/solarnetwork-central/net.solarnetwork.central.datum/defs/sql/postgres
-	# for some reason, plv8 often chokes on the inline comments, so strip them out
-	sed -e '/^\/\*/d' -e '/^ \*/d' postgres-init-plv8.sql |psql -d solarnetwork -U solarnet
-	psql -d solarnetwork -U solarnet -f postgres-init.sql
+eclipseDownload=/var/tmp/eclipse.tgz
+eclipseDownloadMD5=7385aaff4872800153ebc90d8c92e707
+eclipseDownloadHash=
+if [ -e "$eclipseDownload" ]; then
+	eclipseDownloadHash=`md5sum $eclipseDownload |cut -d' ' -f1`
+fi
+if [ "$eclipseDownloadHash" != "$eclipseDownloadMD5" ]; then
+	echo 'Downloading Eclipse Luna...'
+	#wget -c -O $eclipseDownload -nv http://mirrors.ibiblio.org/eclipse/technology/epp/downloads/release/luna/SR1a/eclipse-jee-luna-SR1a-linux-gtk.tar.gz
+	curl -C - -s -S -o $eclipseDownload http://mirrors.ibiblio.org/eclipse/technology/epp/downloads/release/luna/SR1a/eclipse-jee-luna-SR1a-linux-gtk.tar.gz
+fi
+if [ -e "$eclipseDownload" ]; then
+	eclipseDownloadHash=`md5sum $eclipseDownload |cut -d' ' -f1`
+fi
+if [ ! -d ~/eclipse -a -e "$eclipseDownload" ]; then
+	if [ "$eclipseDownloadHash" = "$eclipseDownloadMD5" ]; then
+		echo "Installing Eclipse Luna..."
+		tar -C ~/ -xzf "$eclipseDownload"
+	else
+		echo 'Eclipse Luna not completely downloaded, cannot install.'
+	fi
 fi
 
-psql -d solarnet_unittest -U solarnet_test -c 'select count(*) from solarnet.sn_node' >/dev/null 2>&1
-if [ $? -ne 0 ]; then
-	echo 'Creating solarnet_unittest database tables...'
-	cd ~/git/solarnetwork-central/net.solarnetwork.central.datum/defs/sql/postgres
-	# for some reason, plv8 often chokes on the inline comments, so strip them out
-	sed -e '/^\/\*/d' -e '/^ \*/d' postgres-init-plv8.sql |psql -d solarnet_unittest -U solarnet_test
-	psql -d solarnet_unittest -U solarnet_test -f postgres-init.sql
+if [ ! -d  ~/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings ]; then
+	mkdir -p ~/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings
 fi
+
+# Add Git repos to Eclipse configuration
+if [ ! -e ~/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.egit.core.prefs ]; then
+	echo 'Configuring SolarNetwork git repositories in Eclipse...'
+	cat > ~/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.egit.core.prefs <<EOF
+GitRepositoriesView.GitDirectories=/home/solardev/git/solarnetwork-central/.git\:/home/solardev/git/solarnetwork-common/.git\:/home/solardev/git/solarnetwork-node/.git\:/home/solardev/git/solarnetwork-build/.git\:/home/solardev/git/solarnetwork-external/.git\:
+RepositorySearchDialogSearchPath=/home/solardev/git
+eclipse.preferences.version=1
+EOF
+fi
+
+# Add SolarNetwork target platform configuration
+if [ ! -e ~/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.pde.core.prefs ]; then
+	echo 'Configuring SolarNetwork Eclipse PDE target platform...'
+	cat > ~/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.pde.core.prefs <<EOF
+eclipse.preferences.version=1
+workspace_target_handle=resource\:/solarnetwork-osgi-target/defs/solarnetwork-gemini.target
+EOF
+fi
+
+# Add SolarNetwork debug launch configuration to Eclipse
+if [ ! -e ~/workspace/.metadata/.plugins/org.eclipse.debug.core/.launches/SolarNetwork.launch -a -e /vagrant/SolarNetwork.launch ]; then
+	echo 'Creating SolarNetwork Eclipse launch configuration...'
+	if [ ! -d ~/workspace/.metadata/.plugins/org.eclipse.debug.core/.launches ]; then
+		mkdir -p ~/workspace/.metadata/.plugins/org.eclipse.debug.core/.launches
+	fi
+	cp /vagrant/SolarNetwork.launch ~/workspace/.metadata/.plugins/org.eclipse.debug.core/.launches
+fi
+
+elementIn () {
+  local e
+  for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
+  return 1
+}
+
+addTeamProviderRepo () {
+	local repo
+	local proj
+	repo=`echo $1 |cut -d'/' -f1`
+	proj=`echo $1 |cut -d'/' -f2`
+	echo "Adding $project to Eclipse Team Project Set..."
+	cat >> $2 <<EOF
+<project reference="1.0,https://github.com/SolarNetwork/${repo}.git,develop,${proj}"/>
+EOF
+}
+
+skipProjects=("solarnetwork-build/archiva-obr-plugin" \
+	"solarnetwork-build/net.solarnetwork.pki.sun.security" \
+	"solarnetwork-central/net.solarnetwork.central.user.pki.dogtag" \
+	"solarnetwork-central/net.solarnetwork.central.user.pki.dogtag.test" \
+	"solarnetwork-node/net.solarnetwork.node.config" \
+	"solarnetwork-node/net.solarnetwork.node.setup.developer" )
+# Generate Eclipse Team Project Set of all projects to import
+if [ ! -e ~/SolarNetworkTeamProjectSet.psf ]; then
+	cat > ~/SolarNetworkTeamProjectSet.psf <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<psf version="2.0">
+<provider id="org.eclipse.egit.core.GitProvider">
+EOF
+	
+	cd ~/git
+	projects=`ls -1d */*`
+	for project in $projects; do
+		if elementIn "$project" "${skipProjects[@]}"; then
+			echo "Skipping project $project"
+		else
+			addTeamProviderRepo "$project" ~/SolarNetworkTeamProjectSet.psf
+		fi
+	done
+	
+	cat >> ~/SolarNetworkTeamProjectSet.psf <<EOF
+</provider>
+</psf>
+EOF
+fi
+
+if [ ! -d ~/.fluxbox ]; then
+	mkdir ~/.fluxbox
+fi
+
+# Setup Fluxbox menu
+if [ ! -e ~/.fluxbox/menu ]; then
+	echo 'Configuring Fluxbox menu...'
+	cat > ~/.fluxbox/menu <<EOF
+[begin] (SolarNetwork Dev)
+        [exec] (Eclipse) { ~/eclipse/eclipse -data ~/workspace } <~/eclipse/icon.xpm>
+        [exec] (Firefox) { firefox } </usr/share/pixmaps/firefox.png>
+        [exec] (pgAdminIII) { pgadmin3 } </usr/share/pixmaps/pgadmin3.xpm>
+        [submenu] (Shells) {}
+                [exec] (Bash) { x-terminal-emulator -T "Bash" -e /bin/bash --login} <>
+                [exec] (Dash) { x-terminal-emulator -T "Dash" -e /bin/dash -i} <>
+                [exec] (Sh) { x-terminal-emulator -T "Sh" -e /bin/sh --login} <>
+        [end]
+        [workspaces] (Workspaces)
+        [reconfig] (Reconfigure)
+        [restart] (Restart)
+        [exit] (Exit)
+[end]
+EOF
+fi
+
+# Set Eclipse to launch on Fluxbox startup
+if [ -x ~/eclipse/eclipse -a ! -e ~/.fluxbox/startup ]; then
+	echo 'Configuring Eclipse to start on login...'
+	cat > ~/.fluxbox/startup <<EOF
+#!/bin/sh
+
+xmodmap "/home/solardev/.Xmodmap"
+
+if [ -x ~/eclipse/eclipse ]; then
+	~/eclipse/eclipse -data ~/workspace &
+fi
+
+# Debian-local change:
+#   - fbautostart has been added with a quick hack to check to see if it
+#     exists. If it does, we'll start it up by default.
+which fbautostart > /dev/null
+if [ $? -eq 0 ]; then
+	fbautostart
+fi
+
+exec fluxbox
+EOF
+fi
+
+# Install EGit
+# ls -1d ~/eclipse/features/org.eclipse.egit* >/dev/null 2>&1
+# if [ $? -ne 0 -a -x ~/eclipse/eclipse ]; then
+# 	echo 'Installing EGit...'
+# 	~/eclipse/eclipse -application org.eclipse.equinox.p2.director \
+# 		-repository http://download.eclipse.org/releases/luna \
+# 		-installIU org.eclipse.egit.feature.group \
+# 		-tag AddEGit \
+# 		-destination ~/eclipse/ -profile SDKProfile
+# fi
+
+cat <<EOF
+
+SolarNetwork development environment setup complete. Log into the VM as
+solardev:solardev and Eclipse will launch automatically. Right-click on
+the desktop to access a menu of other options. 
+EOF
