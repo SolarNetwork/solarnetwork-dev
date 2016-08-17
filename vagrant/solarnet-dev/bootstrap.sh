4f97af073083a8afef58c49cfd055a215d7abfcc
Matt Magoffin <matt@solarnetwork.net>
2016-08-18 09:27:37 +1200

    Make Postgres and Java versions more easily configured. Same with hostname. Setup hostname first to avoid excessive warnings from sudo and "unknown host ubuntu-xenial32".

--- vagrant/solarnet-dev/bootstrap.sh
+++ vagrant/solarnet-dev/bootstrap.sh
@@ -1,15 +1,44 @@
 #!/bin/sh
 
+PGVER=9.5
+JAVAVER=8
+HOST=solarnetworkdev.net
+
+grep -q solarnetworkdev /etc/hosts
+if [ $? -ne 0 ]; then
+	echo "Setting up $HOST host"
+	echo $HOST >>/tmp/hostname.new
+	chmod 644 /tmp/hostname.new
+	sudo chown root:root /tmp/hostname.new
+	sudo cp -a /etc/hostname /etc/hostname.bak
+	sudo mv -f /tmp/hostname.new /etc/hostname
+
+	sudo hostname $HOST
+
+	sed "s/^127.0.0.1 localhost$/127.0.0.1 $HOST localhost/" /etc/hosts >/tmp/hosts.new
+	chmod 644 /tmp/hosts.new
+	sudo chown root:root /tmp/hosts.new
+	sudo cp -a /etc/hosts /etc/hosts.bak
+	sudo mv -f /tmp/hosts.new /etc/hosts
+fi
+
+echo 'Updating package cache...'
 sudo apt-get update
+
+echo 'Installing X...'
 sudo apt-get install -y xorg fluxbox
-sudo apt-get install -y postgresql-9.3-plv8 postgresql-contrib-9.3 pgadmin3 git git-flow openjdk-7-jdk librxtx-java
+
+echo 'Installing Postgres $PGVER and Java $JAVAVER...'
+sudo apt-get install -y postgresql-$PGVER postgresql-$PGVER-plv8 postgresql-contrib-$PGVER pgadmin3 git git-flow openjdk-$JAVAVER-jdk librxtx-java
+
+echo 'Installing web browsers...'
 sudo apt-get install -y firefox libwebkitgtk-1.0-0
 
-if [ -e /usr/share/java/RXTXcomm.jar -a -d /usr/lib/jvm/java-7-openjdk-i386/jre/lib/ext \
-		-a ! -e /usr/lib/jvm/java-7-openjdk-i386/jre/lib/ext/RXTXcomm.jar ]; then
+if [ -e /usr/share/java/RXTXcomm.jar -a -d /usr/lib/jvm/java-$JAVAVER-openjdk-i386/jre/lib/ext \
+		-a ! -e /usr/lib/jvm/java-$JAVAVER-openjdk-i386/jre/lib/ext/RXTXcomm.jar ]; then
 	echo 'Linking RXTX JAR to JRE...'
 	sudo ln -s /usr/share/java/RXTXcomm.jar \
-		/usr/lib/jvm/java-7-openjdk-i386/jre/lib/ext/RXTXcomm.jar
+		/usr/lib/jvm/java-$JAVAVER-openjdk-i386/jre/lib/ext/RXTXcomm.jar
 fi
 
 # Add the solardev user if it doesn't already exist, password solardev
@@ -20,39 +49,29 @@ if [ $? -ne 0 ]; then
 	sudo sh -c 'echo "solardev:solardev" |chpasswd'
 fi
 
-grep -q solarnetworkdev /etc/hosts
-if [ $? -ne 0 ]; then
-	echo 'Setting up solarnetworknet.dev host'
-	sed 's/^127.0.0.1 localhost$/127.0.0.1 solarnetworkdev.net localhost/' /etc/hosts >/tmp/hosts.new
-	chmod 644 /tmp/hosts.new
-	sudo chown root:root /tmp/hosts.new
-	sudo cp -a /etc/hosts /etc/hosts.bak
-	sudo mv -f /tmp/hosts.new /etc/hosts
-fi
-
-grep -q plv8.start_proc /etc/postgresql/9.3/main/postgresql.conf
-if [ $? -ne 0 -a -e /etc/postgresql/9.3/main/postgresql.conf ]; then
+grep -q plv8.start_proc /etc/postgresql/$PGVER/main/postgresql.conf
+if [ $? -ne 0 -a -e /etc/postgresql/$PGVER/main/postgresql.conf ]; then
 	echo 'Configuring plv8 global procedure...'
-	sudo sh -c "echo \"plv8.start_proc = 'plv8_startup'\" >> /etc/postgresql/9.3/main/postgresql.conf"
+	sudo sh -c "echo \"plv8.start_proc = 'plv8_startup'\" >> /etc/postgresql/$PGVER/main/postgresql.conf"
 	sudo service postgresql restart
 fi
 
-sudo grep -q solarnet /etc/postgresql/9.3/main/pg_ident.conf
+sudo grep -q solarnet /etc/postgresql/$PGVER/main/pg_ident.conf
 if [ $? -ne 0 ]; then
 	echo 'Configuring Postgres solardev user mapping...'
-	sudo sh -c "echo \"solarnet solardev solarnet\" >> /etc/postgresql/9.3/main/pg_ident.conf"
-	sudo sh -c "echo \"solartest solardev solarnet_test\" >> /etc/postgresql/9.3/main/pg_ident.conf"
+	sudo sh -c "echo \"solarnet solardev solarnet\" >> /etc/postgresql/$PGVER/main/pg_ident.conf"
+	sudo sh -c "echo \"solartest solardev solarnet_test\" >> /etc/postgresql/$PGVER/main/pg_ident.conf"
 	sudo service postgresql restart
 fi
 
-sudo grep -q map=solarnet /etc/postgresql/9.3/main/pg_hba.conf
+sudo grep -q map=solarnet /etc/postgresql/$PGVER/main/pg_hba.conf
 if [ $? -ne 0 -a -e /vagrant/pg_hba.sed ]; then
 	echo 'Configuring Postgres SolarNetwork peer mapping...'
-	sudo sh -c 'sed -f /vagrant/pg_hba.sed /etc/postgresql/9.3/main/pg_hba.conf > /etc/postgresql/9.3/main/pg_hba.conf.new'
-	sudo chown postgres:postgres /etc/postgresql/9.3/main/pg_hba.conf.new
-	sudo chmod 640 /etc/postgresql/9.3/main/pg_hba.conf.new
-	sudo mv /etc/postgresql/9.3/main/pg_hba.conf /etc/postgresql/9.3/main/pg_hba.conf.bak
-	sudo mv /etc/postgresql/9.3/main/pg_hba.conf.new /etc/postgresql/9.3/main/pg_hba.conf
+	sudo sh -c "sed -f /vagrant/pg_hba.sed /etc/postgresql/$PGVER/main/pg_hba.conf > /etc/postgresql/$PGVER/main/pg_hba.conf.new"
+	sudo chown postgres:postgres /etc/postgresql/$PGVER/main/pg_hba.conf.new
+	sudo chmod 640 /etc/postgresql/$PGVER/main/pg_hba.conf.new
+	sudo mv /etc/postgresql/$PGVER/main/pg_hba.conf /etc/postgresql/$PGVER/main/pg_hba.conf.bak
+	sudo mv /etc/postgresql/$PGVER/main/pg_hba.conf.new /etc/postgresql/$PGVER/main/pg_hba.conf
 	sudo service postgresql restart
 fi
 
