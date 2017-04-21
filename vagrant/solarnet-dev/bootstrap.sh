16493edc7b8e91c0533b25c41cd11fbf655ebb41
Matt Magoffin <matt@solarnetwork.net>
2017-04-22 10:39:12 +1200

    Create the swapfile during setup; Eclipse can crash when running the platform under tight memory situations and having swap available isn’t a bad thing by default.

--- vagrant/solarnet-dev/bootstrap.sh
+++ vagrant/solarnet-dev/bootstrap.sh
@@ -23,6 +23,16 @@ if [ $? -ne 0 ]; then
 	sudo mv -f /tmp/hosts.new /etc/hosts
 fi
 
+grep -q '/swapfile' /etc/fstab
+if [ $? -ne 0 ]; then
+	echo -e '\nCreating swapfile...'
+	sudo fallocate -l 1G /swapfile
+	sudo chmod 600 /swapfile
+	sudo mkswap /swapfile
+	sudo swapon /swapfile
+	echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+fi
+
 echo -e '\nUpdating package cache...'
 sudo apt-get update
 
