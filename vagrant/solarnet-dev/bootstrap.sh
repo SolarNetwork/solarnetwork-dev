e4ad3c5d800fa27be26dd8814f57279eefa8494c
Matt Magoffin <matt@solarnetwork.net>
2017-04-21 18:51:46 +1200

    Only install web browsers if desktop packages are also installed.

--- vagrant/solarnet-dev/bootstrap.sh
+++ vagrant/solarnet-dev/bootstrap.sh
@@ -35,8 +35,10 @@ fi
 echo "Installing Postgres $PGVER and Java $JAVAVER..."
 sudo apt-get install -y postgresql-$PGVER postgresql-$PGVER-plv8 postgresql-contrib-$PGVER pgadmin3 git git-flow openjdk-$JAVAVER-jdk librxtx-java
 
-echo 'Installing web browsers...'
-sudo apt-get install -y firefox libwebkitgtk-1.0-0
+if [ -n "$DESKTOP_PACKAGES" ]; then
+	echo -e '\nInstalling web browsers...'
+	sudo apt-get install -y firefox libwebkitgtk-1.0-0
+fi
 
 if [ -e /usr/share/java/RXTXcomm.jar -a -d /usr/lib/jvm/java-$JAVAVER-openjdk-i386/jre/lib/ext \
 		-a ! -e /usr/lib/jvm/java-$JAVAVER-openjdk-i386/jre/lib/ext/RXTXcomm.jar ]; then
